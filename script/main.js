51bd4d8fb1932ea733aae51bb38e1df90175cecd
tgt26 <38292380+tgt26@users.noreply.github.com>
2018-08-31 12:08:31 -0400

    highlight nav item when in that section

--- script/main.js
+++ script/main.js
@@ -1,24 +1,41 @@
 /*jshint esversion: 6 */
 
 // Navigation Bar
-$("#resp-btn").click(function() {
+$("#resp-btn").click(function () {
 	$("nav ul").toggleClass("resp-nav");
 });
 
 // Smooth Scrolling to section
 let navHeight = $("nav").outerHeight(); //Get the height of the nav
-$("nav li a").click(function(e) {
+$("nav li a").click(function (e) {
+	e.preventDefault();
 	// Get href from every link
 	let linkHrefs = $(this).attr("href");
-	$("html, body").animate(
-		{
+	$("html, body").animate({
 			scrollTop: $(linkHrefs).offset().top - navHeight //Substract height of na
 		},
 		1500
 	);
 	$("nav ul").toggleClass("resp-nav");
-	e.preventDefault();
+
 });
+
+//Highlight current section
+$(window).scroll(function () {
+	let scrollbarLocation = $(this).scrollTop();
+	//console.log(scrollbarLocation);
+	$('nav li a').each(function () {
+		let sectionOffset = $(this.hash).offset().top;
+
+		if (sectionOffset <= scrollbarLocation) {
+			$(this).addClass('active');
+			$(this).parent().siblings().children().removeClass('active'); //remove active class from other a tags
+		}
+	});
+
+});
+
+
 //----------------------------------------------------------------------------
 
 // Embeded Youtube Videos
@@ -33,12 +50,12 @@ for (let i = 0; i < youtube.length; i++) {
 	image.src = source;
 	image.addEventListener(
 		"load",
-		(function() {
+		(function () {
 			youtube[i].appendChild(image);
 		})(i)
 	);
 
-	youtube[i].addEventListener("click", function() {
+	youtube[i].addEventListener("click", function () {
 		let iframe = document.createElement("iframe");
 
 		iframe.setAttribute("frameborder", "0");
@@ -46,8 +63,8 @@ for (let i = 0; i < youtube.length; i++) {
 		iframe.setAttribute(
 			"src",
 			"https://www.youtube.com/embed/" +
-				this.dataset.embed +
-				"?rel=0&showinfo=0&autoplay=1"
+			this.dataset.embed +
+			"?rel=0&showinfo=0&autoplay=1"
 		);
 
 		this.innerHTML = "";
@@ -61,13 +78,13 @@ for (let i = 0; i < youtube.length; i++) {
 let showVideo = document.querySelectorAll(".show-video");
 
 for (let i = 0; i < showVideo.length; i++) {
-	showVideo[i].addEventListener("click", function() {
+	showVideo[i].addEventListener("click", function () {
 		youtube[i].style.display = "block";
 	});
 }
 
 // Show tooltip when clicked
 let toolTip = document.querySelector(".tooltip");
-toolTip.addEventListener("click", function() {
+toolTip.addEventListener("click", function () {
 	document.querySelector(".tooltip-text").style.visibility = "visible";
 });
